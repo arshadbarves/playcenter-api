56b5fa2bc95dc1660e01e31a0e9f4578bcb01c4e
Arshad Barves <39230484+arshadbarves@users.noreply.github.com>
2023-03-18 10:46:35 +0530

    Added gunicorn==20.1.0

--- playcenter_api/urls.py
+++ playcenter_api/urls.py
@@ -15,13 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from home import views
-
-router = routers.DefaultRouter()
-router.register(r'', views.WelcomeView, 'welcome')
-
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
